0769bee6a9ff887e606302049a97f74ecdd28556
D0myyy <201152420+D0myyy@users.noreply.github.com>
2025-03-08 16:21:31 +0200

    Add files via upload

--- Functions.js
+++ Functions.js
@@ -1,18 +1,34 @@
 const display = document.getElementById("text");
 
-function displayButton(input){
-    if(display.value != "Error"){
-        display.value += input;
-    } else {
-        display.value = "";
-        display.value += input;
+function displayButton(input) {
+    const lastChar = display.value.slice(-1);
+    if (input >= '0' && input <= '9') {
+        if (display.value !== "Error" && display.value.length < 14) {
+            display.value += input;
+        } else if (display.value.length < 14) {
+            display.value = "";
+            display.value += input;
+        }
+    } else if (['+', '-', '*', '/'].includes(input)) {
+        if (display.value !== "Error" && display.value.length < 14 && !['+', '-', '*', '/'].includes(lastChar)) {
+            display.value += input;
+        }
     }
-    
 }
 
 function calculate(){
     try{
-        display.value = eval(display.value);
+        if(display.value == "Error"){
+            display.value = "";
+        } else{
+            if(display.value != ""){
+                display.value = eval(display.value);
+                while(display.value.length > 14){
+                    display.value = display.value.slice(0,-1);
+                }
+            }
+        }
+        
     }
     catch(error){
         display.value = "Error";
